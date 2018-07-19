616cdfedbca55581c455ad540c76523423bbde1c
Miome <miome.geo@yahoo.com>
2018-07-19 15:47:43 -0400

    Renamed content pack, add new code pack

--- HybridCrops/ModEntry.cs
+++ HybridCrops/ModEntry.cs
@@ -1,10 +1,58 @@
 ﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
 namespace HybridCrops
 {
-    public class MyClass
+    /// <summary>The mod entry point.</summary>
+    public class ModEntry : Mod, IAssetEditor
     {
-        public MyClass()
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
         {
+            InputEvents.ButtonPressed += this.InputEvents_ButtonPressed;
+        }
+
+
+        /*********
+        ** Private methods
+        *********/
+        /// <summary>The method invoked when the player presses a controller, keyboard, or mouse button.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event data.</param>
+        private void InputEvents_ButtonPressed(object sender, EventArgsInput e)
+        {
+            // ignore if player hasn't loaded a save yet
+            if (!Context.IsWorldReady)
+                return;
+
+            // print button presses to the console window
+            this.Monitor.Log($"{Game1.player.Name} pressed {e.Button}.");
+        }
+        public bool CanEdit<T>(IAssetInfo asset)
+        {
+
+            if (asset.AssetNameEquals("Data/CraftingRecipes"))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Edit<T>(IAssetData asset)
+        {
+            if (asset.AssetNameEquals("Data/CraftingRecipes"))
+            {
+                asset.AsDictionary<string, string>().Data["Pumpkin Seeds"] = "276 1/Field/490 5/false/s Farming 1";
+            }
         }
     }
 }
